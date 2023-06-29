69f194848afe49a0e382b6606e175fbdecb0b368
njogued <njogued@gmail.com>
2023-06-29 23:26:16 +0000

    Pascal Triangle Recursion

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -13,6 +13,4 @@ def print_triangle(triangle):
 
 
 if __name__ == "__main__":
-    print_triangle(pascal_triangle(2))
-    print_triangle(pascal_triangle(3))
     print_triangle(pascal_triangle(5))
