bd4be6b47de7d69a55911d51aefe24dbc6f5d248
njogued <njogued@gmail.com>
2023-08-11 02:13:12 +0300

    Send a request to Starwars API using javascript

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const episode = process.argv[2];
+const episodeEndpoint = "https://swapi-api.alx-tools.com/api/films" + episode;
+
+function sendRequest(characterList, index) {
+  // Base case: stop when index reaches the length of characterList
+  if (characterList.length === index) {
+    return;
+  }
+  // Make a request for the character at the current index
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      // Log error if there's an issue with the request
+      console.log(error);
+    } else {
+      // Log the name of the character from the response body
+      console.log(JSON.parse(body).name);
+      // Recursively call sendRequest for the next character
+      sendRequest(characterList, index + 1);
+    }
+  });
+}
+
+request(episodeEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    sendRequest(characters, 0);
+  }
+});
